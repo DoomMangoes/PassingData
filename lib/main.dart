e732a0d40723d26f1682c42a43497e4cb3726e64
MARC_DIONIS_DUMANGAS <marcdionis.dumangas.20@usjr.edu.ph>
2022-11-06 11:34:43 +0000

    actual commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,35 +7,115 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        // Application name
+        debugShowCheckedModeBanner: false,
+        home: FirstPage());
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class FirstPage extends StatelessWidget {
+  String? input;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor: Colors.greenAccent,
+        title: Text('First Page'),
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      body: SafeArea(
+          child: Center(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+            SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: '[Some Hint Text]',
+                    ),
+                    onChanged: (text) {
+                      input = text;
+                    }),
+              ),
+            ),
+            SizedBox(height: 10),
+            FlatButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(28.0),
+              ),
+              height: 75,
+              child: Container(
+                height: 75,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  gradient: LinearGradient(
+                    colors: <Color>[
+                      Colors.greenAccent,
+                      Colors.blueAccent,
+                    ],
+                  ),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      "Send",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SecondPage(input: input)));
+              },
+            ),
+          ]))),
     );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  String? input;
+
+  SecondPage({this.input});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.greenAccent,
+          title: Text('Second Page'),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                input!,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ]));
   }
 }
